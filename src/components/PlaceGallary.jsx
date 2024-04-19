ecdeff069e36d75d22a7be21ef1144ac5833710d
sujal gupta <sujalgupta5656@gmail.com>
2024-04-19 11:34:57 +0530

    changes made in css

--- src/components/PlaceGallary.jsx
+++ src/components/PlaceGallary.jsx
@@ -63,7 +63,7 @@ const PlaceGallary = (props) => {
       </div>
       <button
         onClick={() => setShowAllPhotos(true)}
-        className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500"
+        className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow-md shadow-gray-500"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
